207bc8994e4f879ef02df563c8927d623af8594d
pvberkel <pvberkel50505@gmail.com>
2022-11-10 21:14:52 +0100

    Math proxy where products route now also return time.

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 env = {
-  SERVERIP:   'europe-west1-hello-smullbox-01.cloudfunctions.net/smulbox',
-  SERVERPORT: '443',
-  FACEBOOKAPPID: '429034655516014',
-  REQTIMEOUT: '5000',
-  REPEATDELAY: '30000'
+SERVERIP: 'europe-west1-hello-smullbox-01.cloudfunctions.net/smulbox',
+SERVERPORT: 443,
+FACEBOOKAPPID: 429034655516014,
+REQTIMEOUT: 5000,
+REPEATDELAY: 30000
 }
