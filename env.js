aedcf6da0d310c00164ae4e448c8258c6a82d05e
pvberkel <pvberkel50505@gmail.com>
2025-01-24 22:33:44 +0100

    Switch to go function

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 env = {
-SERVERIP: 'europe-west1-hello-smullbox-02.cloudfunctions.net/smulbox',
+SERVERIP: 'europe-west1-hello-smullbox-02.cloudfunctions.net/smulbox-go',
 SERVERPORT: 443,
 FACEBOOKAPPID: 429034655516014,
 REQTIMEOUT: 5000,
